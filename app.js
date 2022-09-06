88e6c55a7aa2e27d4032f6c907c3795a6c8b5ca7
Jimson-Botterfly <opeyemiabdulateef406@gmail.com>
2022-09-06 21:52:32 +0100

    Add procfile and update ports

--- app.js
+++ app.js
@@ -329,8 +329,13 @@ requestedId = req.params.postId
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(port, function(){
+console.log("Server started sucessfully");
+}) 
+
 //C:\Program Files\MongoDB\Server\5.0\data\   
